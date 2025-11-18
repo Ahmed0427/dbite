0bc045cd14b44a6a97b36d82c47d9d13fddf74b6
ahmed0427 <ahmedabdallah1090@gmail.com>
2025-11-18 20:07:26 +0200

    feat: commen constants

---
+++ src/common.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+static constexpr uint8_t BNODE_INTERNAL = 1;
+static constexpr uint8_t BNODE_LEAF = 2;
+
+static constexpr size_t BTREE_PAGE_SIZE = 4 * 1024;
+
+static constexpr size_t NODE_TYPE_SIZE = 1;
+static constexpr size_t HEADER_KEY_COUNT_SIZE = 2;
+static constexpr size_t PAGE_HEADER_SIZE =
+    NODE_TYPE_SIZE + HEADER_KEY_COUNT_SIZE;
+
+static constexpr size_t PTR_SIZE = 4;
+static constexpr size_t OFFSET_SIZE = 2;
+
+static constexpr size_t KEY_SIZE_FIELD_SIZE = 2;
+static constexpr size_t VALUE_SIZE_FIELD_SIZE = 2;
+static constexpr size_t ENTRY_HEADER_SIZE =
+    KEY_SIZE_FIELD_SIZE + VALUE_SIZE_FIELD_SIZE;
+
+static constexpr size_t MAX_ENTRY_SIZE = BTREE_PAGE_SIZE - PAGE_HEADER_SIZE -
+                                         PTR_SIZE - OFFSET_SIZE -
+                                         ENTRY_HEADER_SIZE;
